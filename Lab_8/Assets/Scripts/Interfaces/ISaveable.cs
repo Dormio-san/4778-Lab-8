0745714caf8128a055095a5253b61948589f3c69
ALabroo18 <102484252+ALabroo18@users.noreply.github.com>
2024-10-23 15:19:19 -0400

    Update ISaveable.cs

--- Lab_8/Assets/Scripts/Interfaces/ISaveable.cs
+++ Lab_8/Assets/Scripts/Interfaces/ISaveable.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using LitJson;
+using System.Linq;
 
 public interface ISaveable 
 {
@@ -15,4 +16,31 @@ public static class SavingService
     private const string SCENES_KEY = "scenes";
     private const string OBJECTS_KEY = "objects";
     private const string SAVEID_KEY = "$saveID";
+    public static void SaveGame(string fileName)
+    {
+        var result = new JsonData();
+        var allSaveableObjects = Object.FindObjectsOfType<MonoBehaviour>().OfType<ISaveable>();
+        if (allSaveableObjects.Count() > 0)
+        {
+            var savedObjects = new JsonData();
+            foreach (var saveableObject in allSaveableObjects)
+            {
+                var data = saveableObject.SavedData;
+                if (data.IsObject)
+                {
+                    data[SAVEID_KEY] = saveableObject.SaveID;
+                    savedObjects.Add(data);
+                }
+                else
+                {
+                    var behaviour = saveableObject as MonoBehaviour;
+                    Debug.LogWarningFormat(behaviour, "{0}'s save data is not a dictionary. The object was not saved.", behaviour.name );
+                }
+            }
+            result[OBJECTS_KEY] = savedObjects;
+        }
+        else { Debug.LogWarningFormat("The scene did not include any saveable objects."); }
+     }
 }
+
+
