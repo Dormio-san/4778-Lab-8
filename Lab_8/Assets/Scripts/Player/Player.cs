5bfbb6d02cb22de3080b64dfb63a133f3598cc0c
XxTimaeusxX <149029398+XxTimaeusxX@users.noreply.github.com>
2024-10-28 03:51:15 -0400

    lab9 update

---
+++ Lab_8/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,89 @@
+using LitJson;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player :TransformSave, ISubject 
+{
+    private List<IObserver> observers = new List<IObserver>();
+    public PlayerScore playerScore { get; private set; }
+    public PlayerHealth playerHealth { get; private set; }
+    private IObserver gameOverObserver;
+   
+
+    // Constructor to initialize the player's score and health.
+    public Player()
+    {
+        playerScore = new PlayerScore();
+        playerHealth = new PlayerHealth();
+    }
+
+    // Attach (add) an observer to the list of observers.
+    public void Attach(IObserver observer)
+    {
+        observers.Add(observer);
+    }
+
+    // Detach (remove) an observer from the list of observers.
+    public void Detach(IObserver observer)
+    {
+        observers.Remove(observer);
+    }
+
+    public void Notify()
+    {
+        // Notify all observers and let them do their logic that depends on the player's state.
+        foreach (IObserver observer in observers)
+        {
+            // If the observer is the GameManager, exit.
+            if (observer is GameOverUI)
+            {
+                gameOverObserver = observer;
+                Detach(observer);
+                return;
+            }
+            observer.OnNotify();
+        }
+    }
+
+    // Change the player's score and notify the observers.
+    public void ChangeScore(int scoreChange)
+    {
+        playerScore.ChangeScore(scoreChange);
+        Notify();
+    }
+
+    // Change the player's health and notify the observers.
+    public void ChangeHealth(int healthChange)
+    {
+        playerHealth.ChangeHealth(healthChange);
+        Notify();
+
+        // If the health is at or below 0, notify the game over observer, which ends the game.
+        if (playerHealth.Health <= 0)
+        {
+            gameOverObserver.OnNotify();
+        }
+    }
+
+    // Used when game begins to reset the score and health to 0 and 3, respectively.
+    public void BeginGameReset()
+    {
+        playerScore.ResetScore();
+        playerHealth.ResetHealth();
+        Notify();
+    }
+
+    public override string SaveID
+    {
+        get => base.SaveID; // Access the inherited SaveID property directly
+        set => base.SaveID = value;
+    }
+
+    public override JsonData SavedData => base.SavedData;
+    
+
+    // Implementing LoadFromData from ISaveable
+    public override void LoadFromData(JsonData data) => base.LoadFromData(data);
+    
+    
+}
